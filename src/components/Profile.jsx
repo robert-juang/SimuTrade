65aeaa3271e70b432cf4033f8919c87f6f7ca26a
robert-juang <zdjiang20001225@gmail.com>
2023-07-27 00:03:37 -0400

    test

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import "../styles/Profile.css"
 import { useAuth } from "../contexts/AuthContext"
 
@@ -7,22 +7,20 @@ function Profile() {
   const [username, setUsername] = useState()
   const { currentUser, updatePassword, updateEmail } = useAuth()
 
-  useEffect(()=>{
-    console.log(currentUser); 
-  },[])
+  useEffect(() => {
+    console.log(currentUser);
+  }, [])
 
   return (
-      <div class="settings">
-          <h3>Account Settings</h3>
-          <div class="information">
-              <span>Account Information</span>
-          </div>
-          <div class="contents">
-              <span id="C1">Email: {currentUser.email}</span>
-              <span id="C1">Date Created: add date here</span>
-              <span id="C1">Number of Simulations Completed: Number of Sim</span>
-          </div>
+    <div class="settings">
+      <h3>My Account</h3>
+
+      <div class="contents">
+        <span id="C1">Email: {currentUser.email}</span>
+        <span id="C1">Date Created: add date here</span>
+        <span id="C1">Number of Simulations Completed: Number of Sim</span>
       </div>
+    </div>
   )
 }
 
